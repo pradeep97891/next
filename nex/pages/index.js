bd48090451703e2e140a86789b40fe1bf59cf153
pradeep97891 <pradeep97.rx@gmail.com>
2020-06-01 10:34:00 +0530

    commit changes

--- nex/pages/index.js
+++ nex/pages/index.js
@@ -4,7 +4,8 @@ import Layout from '../components/layout';
 const Index = (props) => (
     <Layout>
         <div>
-            <h1>Welcome to Plot</h1>
+            <h1>Welcome to git </h1>
+            <h2>Changing</h2>
             <button className="">CLick</button>
         </div>
     </Layout>
